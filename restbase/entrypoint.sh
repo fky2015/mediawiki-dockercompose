cf8a42e05d7c7fd33165c8f3647f02f2acae4a66
FKYnJYQ <loveress01@gmail.com>
2018-08-11 14:32:53 +0800

    update restbase

---
+++ restbase/entrypoint.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+set -e
+
+if [ ! -h config.yaml ]; then
+	echo >&2 "Copying example 'config.example.yaml' into data volume, creating symbolic link."
+
+	if [ ! -e /data/config.yaml ]; then
+		mv config.example.yaml /data/config.yaml
+	fi
+
+	ln -s "/data/config.yaml" config.yaml
+fi
+
+
+exec "$@"
